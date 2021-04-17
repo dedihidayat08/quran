b7032cd0fb1b8041f28601f56d2885b4722a81e8
dedihidayat08 <82642577+dedihidayat08@users.noreply.github.com>
2021-04-17 07:31:43 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react";
 const Footer: FC = () => {
   return (
     <footer className="select-none mt-5 shadow-lg bg-primary rounded px-3 py-2 text-xs text-white text-center">
-      &copy; 2021 &middot; Muhammad Syifa
+      &copy; 2021 &middot; Easy Digital
     </footer>
   );
 };
