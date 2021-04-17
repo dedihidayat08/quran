0c809960c4335d7679bc22db11dd20f3d7c2d36a
dedihidayat08 <82642577+dedihidayat08@users.noreply.github.com>
2021-04-18 05:58:00 +0700

    Update and rename bipandu to bipandu.tsx

---
+++ pages/bipandu.tsx
@@ -0,0 +1,29 @@
+import Head from "../components/Head";
+import Layout from "../components/Layout";
+import LogoAlIhsan from "../components/LogoAlIhsan";
+import SquareMenu from "../components/SquareMenu";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head/>
+      <div className="mb-5 flex flex-wrap justify-center content-center">
+        <div className="w-full sm:w-auto flex justify-center">
+          <LogoAlIhsan size={64}/>
+        </div>
+        <div className="w-full sm:w-auto px-3 select-none text-center">
+          <h1 className="font-bold text-3xl text-primary">Bipandu Apps</h1>
+          <p className="text-sm font-semibold text-oxford-blue">Kumpulan aplikasi islami dan informasi Kepanduan.</p>
+        </div>
+      </div>
+      <div className="w-full grid grid-cols-2 grid-flow-row-dense md:grid-cols-3 gap-4">
+        <div className=""><SquareMenu href="/al-quran" icon="/icon-quran.svg" label="Al-Qur'an"/></div>
+        <div className=""><SquareMenu href="/prayer-times" icon="/icon-salat-clock.svg" label="Waktu Salat"/></div>
+        <div className=""><SquareMenu href="https://dedihidayat.id/bipandu-apps/" icon="/pandu-logo.svg" label="Bipandu"/></div>
+        <div className=""><SquareMenu href="/islamic-calendar" icon="/icon-islamic-calendar.svg" label="Kalender Islam"/></div>
+        <div className=""><SquareMenu href="/duas" icon="/icon-pray.svg" label="Kumpulan Doa"/></div>
+        <div className=""><SquareMenu href="/zakat-calculator" icon="/icon-calculator-zakat.svg" label="Kalkulator Zakat"/></div>
+      </div>
+    </Layout>
+  );
+}
